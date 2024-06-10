b1f10f250888462d7ccf23d8fb94fa5927263c47
andrea-bertaglia <andreabertaglia@gmail.com>
2024-06-10 16:34:59 +0200

    fix: refactoring blade pages

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,18 +1,9 @@
-<!DOCTYPE html>
-<html lang="en">
+@extends('layouts.app')
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+@section('title')
+    Movies
+@endsection
 
-    @vite('resources/js/app.js')
-</head>
-
-<body>
-    <h1>Movies</h1>
-
-</body>
-
-</html>
+@section('content')
+    <h3>questo è home.blade.php</h3>
+@endsection
--- resources/views/movies.blade.php
+++
@@ -1,9 +0,0 @@
-@extends('layouts.app')
-
-@section('title')
-    Movies
-@endsection
-
-@section('content')
-    <h3>questo è movies.blade.php</h3>
-@endsection
