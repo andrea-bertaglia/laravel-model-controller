88c2eac513b813568f54d3b007372a609209a43f
andrea-bertaglia <andreabertaglia@gmail.com>
2024-06-10 16:55:45 +0200

    feat: dynamic cards with bootstrap layout

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,14 +5,14 @@
 @endsection
 
 @section('content')
-    <h1 class="text-center py-3">Movies</h1>
+    <h1 class="text-center py-3">Movie's list</h1>
     <div class="container">
         <div class="row">
-            <div class="col-3">
-
-                @include('partials.movie-card')
-
-            </div>
+            @foreach ($movies as $movie)
+                <div class="col-3 g-5">
+                    @include('partials.movie-card')
+                </div>
+            @endforeach
         </div>
     </div>
 @endsection
--- resources/views/partials/movie-card.blade.php
+++ resources/views/partials/movie-card.blade.php
@@ -1,5 +1,5 @@
 {{-- card con papera placeholder --}}
-<div class="card" style="width: 18rem;">
+{{-- <div class="card" style="width: 18rem;">
     <img class="w-100" src="{{ Vite::asset('resources/img/duck.png') }}" class="card-img-top" alt="...">
     <div class="card-body">
         <div class="card-text">
@@ -9,6 +9,23 @@
                 <li class="list-group-item">nazione</li>
                 <li class="list-group-item">data</li>
                 <li class="list-group-item">voto</li>
+            </ul>
+        </div>
+    </div>
+</div> --}}
+
+{{-- card dinamica --}}
+<div class="card h-100" style="width: 18rem;">
+    <img class="w-100" src="{{ $movie['image'] }}" class="card-img-top" alt="...">
+    <div class="card-body">
+        <div class="card-text">
+            <h5 class="fw-bold">{{ $movie['title'] }}</h5>
+            <small>{{ $movie['original_title'] }}</small>
+            <ul class="list-group list-group-flush pt-4">
+                <li class="list-group-item">Nazionalità: <span class="fw-bold">{{ $movie['nationality'] }}</span></li>
+                <li class="list-group-item">Data: <span class="fw-bold">{{ $movie['date'] }}</span></li>
+                <li class="list-group-item">Voto: <span class="fw-bold">{{ $movie['vote'] }}</span></li>
+                </li>
             </ul>
         </div>
     </div>
