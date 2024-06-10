e426ed47009831bb7d25e1eff8f057c00f93645e
andrea-bertaglia <andreabertaglia@gmail.com>
2024-06-10 16:16:42 +0200

    feat: view creation

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -8,8 +9,10 @@
 
     @vite('resources/js/app.js')
 </head>
+
 <body>
-    <h1>My Laravel-Vite Template</h1>
-    <img src="{{ Vite::asset('resources/img/duck.png') }}" alt="Testo">
+    <h1>Movies</h1>
+
 </body>
+
 </html>
---
+++ resources/views/movies.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.app')
+
+@section('title')
+    Movies
+@endsection
+
+@section('content')
+    <h3>questo è movies.blade.php</h3>
+@endsection
